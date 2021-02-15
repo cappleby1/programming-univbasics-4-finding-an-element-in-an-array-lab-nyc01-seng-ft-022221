9a556e0180cfc727438c37316fa0102131570353
Colten Appleby <colten96@gmail.com>
2021-02-15 02:41:07 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
   array.times do |index|
-    if array[index] == value_to_find:
+    if array[index] == value_to_find
       return index
   end
   return nil
