05223edb9906d670967452506e715e8dad24da3d
Colten Appleby <colten96@gmail.com>
2021-02-15 02:40:55 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,8 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  array.times do |index|
+    if array[index] == value_to_find:
+      return index
+  end
+  return nil
 end
